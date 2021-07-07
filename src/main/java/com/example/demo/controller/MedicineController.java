a98157039ca65bca7ada897aaacd226508f2be17
meenamudaliar <76042713+meenamudaliar@users.noreply.github.com>
2021-07-07 20:12:05 +0530

    Update MedicineController.java

--- src/main/java/com/example/demo/controller/MedicineController.java
+++ src/main/java/com/example/demo/controller/MedicineController.java
@@ -29,7 +29,7 @@ import com.example.demo.repository.MedicineRepository;
 
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/")
 
